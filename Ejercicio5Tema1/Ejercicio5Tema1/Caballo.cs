bbba85c338ec9216a4984c15d61333ae120acaa2
AdrianAlonsoMontes644 <alomontes.adrin@gmail.com>
2022-11-07 13:55:40 +0100

    en proceso

---
+++ Ejercicio5Tema1/Ejercicio5Tema1/Caballo.cs
@@ -0,0 +1,64 @@
+﻿/*Se recomienda realizar POO de forma que caballo sea una clase que al menos tenga 
+el método correr y la propiedad (variable con set/get) posición. El array inicial será de objetos tipo caballo.
+Nota: De cara a realizar pruebas de este juego, se recomienda quitar la aleatoriedad temporalmente 
+para forzar a que varios caballos lleguen a un tiempo y ver que solo uno es el que “cruza” la meta. */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio5Tema1
+{
+    internal class Caballo
+    {
+        private int x;
+        public int X
+        {
+            set
+            {
+                x = value;
+            }
+            get
+            {
+                return x;
+            }
+        }
+
+        private int y;
+        public int Y
+        {
+            set
+            {
+                y = value;
+            }
+            get
+            {
+                return y;
+            }
+        }
+
+        Random random = new Random();
+        public void correr()
+        {
+            for (int y = 0; y < 5; y=y+3)
+            {
+                Console.SetCursorPosition(0, y);
+                Console.WriteLine("/T=T`");
+            }
+            while (x < 50)
+            {
+                x = X + (int)random.Next() * 5;
+                try
+                {
+                    Console.SetCursorPosition(x, y);
+                    Console.WriteLine("/T=T`");
+                }catch (ArgumentOutOfRangeException)
+                {
+
+                } 
+            }
+        }
+    }
+}
---
+++ Ejercicio5Tema1/Ejercicio5Tema1/Program.cs
@@ -0,0 +1,29 @@
+﻿/*Realiza en consola el juego de carreras de caballos con al menos 5 caballos (haz un array de hilos) pero teniendo en cuenta que ahora cada caballo es un thread. 
+ El usuario hace su apuesta y luego empieza la carrera de caballos de forma que cada uno se mueve una distancia aleatoria y “duerme” un tiempo aleatorio. 
+ Al empezar la carrera el Main (o la función inicial) se queda en espera. Una vez que un caballo llega a la meta todos paran y el main continúa indicando el caballo ganador y si el usuario ha ganado. 
+ Se permitirá la repetición del juego. No uses expresiones lambda en este ejercicio. 
+*/
+using Ejercicio5Tema1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Caballo[] caballos = new Caballo[5];
+        int elegido;
+        do
+        {
+            Console.WriteLine("Bienvenido al hipódromo digital, haga su apuesta a uno de los 5 caballos designados");
+            elegido = Convert.ToInt32(Console.ReadLine());
+            if(elegido <= 0 || elegido > 5)
+            {
+                Console.Clear();
+                Console.WriteLine("Esa no es una de las opciones");
+            }
+        }while(elegido <= 0 || elegido>5);
+
+        Caballo c = new Caballo();
+        c.correr();
+    }
+
+}
